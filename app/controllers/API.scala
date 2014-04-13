0e848674252078855ae2ce6c762ef0e3649fe67f
Étienne Vallette d'Osia <evo@zengularity.com>
2014-04-13 18:02:49 +0200

    api: 'update' now use JSON

--- app/controllers/API.scala
+++ app/controllers/API.scala
@@ -6,6 +6,7 @@ import scala.concurrent.Future
 import play.api._
 import play.api.mvc._
 import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 import models.Log
 import engine.Players
@@ -22,20 +23,32 @@ object API extends Controller {
     }
   }
 
-  def update(email: String, diff: Int, msg: String, by: String) = Action.async { implicit req =>
-    Players.findByEmail(email).flatMap {
-      case Some(player) =>
-        val log = Log.create(player, diff, msg, Some(by))
-        Players.updatePoints(player, log).map { player =>
-          Ok(Json.obj(
-            "email" -> email,
-            "points" -> player.points
-          ))
+  def update(email: String) = Action.async(parse.json) { implicit req =>
+    val reader = (
+      (__ \ "diff").read[Int] ~
+      (__ \ "msg").read[String] ~
+      (__ \ "by").read[String]
+    ).tupled
+    reader.reads(req.body).fold(
+      err => Future.successful {
+        BadRequest(Json.obj("error" -> "Bad request"))
+      },
+      { case (diff, msg, by) =>
+        Players.findByEmail(email).flatMap {
+          case Some(player) =>
+            val log = Log.create(player, diff, msg, Some(by))
+            Players.updatePoints(player, log).map { player =>
+              Ok(Json.obj(
+                "email" -> email,
+                "points" -> player.points
+              ))
+            }
+          case None => Future.successful {
+            NotFound(Json.obj("error" -> s"Player $email not found"))
+          }
         }
-      case None => Future.successful {
-        NotFound(Json.obj("error" -> s"Player $email not found"))
       }
-    }
+    )
   }
 
 }
