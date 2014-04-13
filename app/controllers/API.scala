044a7cfc14d417efdd8f0df6b9b4536e9d4d433a
Étienne Vallette d'Osia <evo@zengularity.com>
2014-04-13 16:29:16 +0200

    Add API update

--- app/controllers/API.scala
+++ app/controllers/API.scala
@@ -1,11 +1,13 @@
 package controllers
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 import play.api._
 import play.api.mvc._
 import play.api.libs.json._
 
+import models.Log
 import engine.Players
 
 object API extends Controller {
@@ -17,6 +19,22 @@ object API extends Controller {
         "points" -> player.points
       ))
       case None => NotFound(Json.obj("error" -> s"Player $email not found"))
+    }
+  }
+
+  def update(email: String, diff: Int, msg: String, by: String) = Action.async { implicit req =>
+    Players.findByEmail(email).flatMap {
+      case Some(player) =>
+        val log = Log.create(player, diff, msg, Some(by))
+        Players.updatePoints(player, log).map { player =>
+          Ok(Json.obj(
+            "email" -> email,
+            "points" -> player.points
+          ))
+        }
+      case None => Future.successful {
+        NotFound(Json.obj("error" -> s"Player $email not found"))
+      }
     }
   }
 
